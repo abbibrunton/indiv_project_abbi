b97e75ab6d31cfa6bd5001f3edec5b159a02d872
abbibrunton <55387240+abbibrunton@users.noreply.github.com>
2019-11-01 09:26:07 +0000

    added comments to docker-install.sh

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,15 +1,16 @@
 cd ..
+#checking if docker is already installed
 if [ -x "$(command -v docker)" ]; then
 	echo 'docker already installed'
 else
     echo 'docker not already installed. installing...'
     curl https://get.docker.com | sudo bash
 fi
-
+#checking if the project is already running
 if sudo docker ps -a | grep "indiv"; then
 	sudo docker rm -f indiv
 fi
 
-
+#building and running the application
 sudo docker build -t indiv-project:latest .
 sudo docker run -d -p 5000:5000 --name indiv indiv-project
