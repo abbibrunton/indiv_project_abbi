b9dcc393f42b2676fbad6f55962b77d5eac0e6f3
abbibrunton <abbibrunton42@gmail.com>
2019-11-06 14:49:31 +0000

    adding sql branch

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@104.155.64.23/project_db'
+
 app.config['SECRET_KEY'] = '7218a9143c27c16610765205a1b21cb7'
 
 db = SQLAlchemy(app)
