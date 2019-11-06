efa9dbfc07b411801e6fd88c53a894f705b8b195
abbibrunton <55387240+abbibrunton@users.noreply.github.com>
2019-11-06 14:50:15 +0000

    Update models.py

--- application/models.py
+++ application/models.py
@@ -12,7 +12,7 @@ class Users(db.Model, UserMixin):
 	first_name = db.Column(db.String(30), nullable=False)
 	last_name = db.Column(db.String(30), nullable=False)
 	email = db.Column(db.String(150), nullable=False, unique=True)
-	password = db.Column(db.String(50), nullable=False)
+	password = db.Column(db.String(150), nullable=False)
 	posts = db.relationship('Posts', backref='author', lazy=True)
 	flights = db.relationship('Flights', backref='author', lazy=True)
 	accommodation = db.relationship('Accommodation', backref='author', lazy=True)
