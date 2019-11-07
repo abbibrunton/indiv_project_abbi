211ae5a32aa12f6056e63c9e56188c48a9c55994
abbibrunton <55387240+abbibrunton@users.noreply.github.com>
2019-11-07 13:06:19 +0000

    created docker-compose-install.sh

---
+++ scripts/docker-compose-install.sh
@@ -0,0 +1,20 @@
+cd ..
+#checking if docker is already installed
+if [ -x "$(command -v docker)" ]; then
+	echo 'docker already installed'
+else
+    echo 'docker not already installed. installing...'
+    curl https://get.docker.com | sudo bash
+fi
+
+#checking if docker-compose is already installed
+if [ -x "$(command -v docker-compose)" ]; then
+	echo 'docker-compose already installed'
+else
+    echo 'docker-compose not already installed. installing...'
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+#running docker-compose
+sudo docker-compose up -d
