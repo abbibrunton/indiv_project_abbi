a301def2d9128db0717906746327ceb8430bb797
abbibrunton <55387240+abbibrunton@users.noreply.github.com>
2019-11-08 10:34:05 +0000

    added network deletion to docker-compose script

--- scripts/docker-compose-install.sh
+++ scripts/docker-compose-install.sh
@@ -15,6 +15,7 @@ else
     sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
-
+#deleting network
+docker network rm indiv_project_abbi_default
 #running docker-compose
 sudo docker-compose up -d
